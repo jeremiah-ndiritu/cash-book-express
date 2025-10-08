62b7c18f2e20ea7ad11c8a1e44ac20341fccafaa
DigitalizedDev <digitalized.channel@gmail.com>
2025-10-08 12:54:58 +0300

    add an explicit get('/) route

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ app.use(cors());
 app.use(express.json());
 const PORT = process.env.PORT || 5174;
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
 app.post("/api/back", (req, res) => {
   const { transactions, debts } = req.body;
   if (existsSync(transactionsStore)) {
